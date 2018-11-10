1be48d57f88265c343391bf1ef954b15e3df958c
edsueco <eduardoalvesmelo@gmail.com>
2018-11-09 20:09:40 -0400

    comit projeto matriz

---
+++ projetomatriz/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace projetomatriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] nome = new string[2, 2];
+            string conteudo="";
+
+            for (int i = 0; i< 2; i++){
+                for (int j = 0; j < 2; j++){
+                    if (j == 0) Console.WriteLine("INFORME O NOME:");
+                    if (j == 1) Console.WriteLine("INFORME O SOBRNOME:");
+                    nome[i, j] += Console.ReadLine() + " ";
+                    conteudo += nome[i, j];
+                }
+                conteudo += "\n";
+            }
+            Console.WriteLine(conteudo);
+        }
+    }
+}
